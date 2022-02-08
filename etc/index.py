a794c4800f3c85523b5988e04bca3ccf89253447
Hari Acharya <marleyhari8@gmail.com>
2022-02-08 08:48:43 +0545

    whats wrong

--- etc/index.py
+++ etc/index.py
@@ -1,4 +1,7 @@
 #This is just a check of the pytho programming language.
 
-var Helloworld= input("What's your name?")
-print("welcome " + Helloworld)
+from tokenize import String
+
+
+String helloWorld = input("What's your name?") #What's wrong here?
+print("welcome " + helloWorld)
